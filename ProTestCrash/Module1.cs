a6354f16f6633b3dfdfc0ce57f908ea997381e8e
David Wilton <dave_wilton@msn.com>
2022-01-25 12:00:55 +1100

    Add project files.

---
+++ ProTestCrash/Module1.cs
@@ -0,0 +1,73 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProTestCrash
+{
+    internal class Module1 : Module
+    {
+        private static Module1 _this = null;
+
+        public static string ModuleName = "TestProCrash";
+
+        private Module1()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                // how could we limit these to only our addin? They catch issue cross addin
+                var ex = (Exception)e.ExceptionObject;
+                Log("*** Sample Pro Crash! ***", "UnhandledException" + ex.Message);
+
+            };
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                // how could we limit these to only our addin? They catch issue cross addin
+                Log("*** Sample Pro Crash! ***", "UnobservedTaskException" + e.Exception.Message);
+            };
+
+        }
+
+        internal static void Log(string message, [CallerMemberName] string caller = "")
+        {
+            Console.WriteLine("{0}: {1}", caller, message); 
+            MessageBox.Show($"{message}: {caller}");
+        }
+
+        /// <summary>
+        /// Retrieve the singleton instance to this module here
+        /// </summary>
+        public static Module1 Current => _this ?? (_this = (Module1)FrameworkApplication.FindModule("ProTestCrash_Module"));
+
+        #region Overrides
+        /// <summary>
+        /// Called by Framework when ArcGIS Pro is closing
+        /// </summary>
+        /// <returns>False to prevent Pro from closing, otherwise True</returns>
+        protected override bool CanUnload()
+        {
+            //TODO - add your business logic
+            //return false to ~cancel~ Application close
+            return true;
+        }
+
+        #endregion Overrides
+
+    }
+}
---
+++ ProTestCrash/Shared.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+
+namespace ProTestCrash
+{
+    class Shared
+    {
+        public static async Task DoCrash(string caseToRun)
+        {
+            try
+            {
+                switch (caseToRun)
+                {
+                    case "demo1": // fine
+                        // this won't cause a crash
+                        var res2 = await ExampleTask1();
+                        Console.WriteLine(res2);
+                        break;
+
+                    case "demo2": // crash pro
+                        // if we combine QT and async void it crashes pro
+                        ExampleTask2();
+                        break;
+
+                    case "demo3": // crash pro
+                        // this should be safe in .net 4.5? yet it still crashes pro.
+                        // see https://devblogs.microsoft.com/pfxteam/task-exception-handling-in-net-4-5/
+                        ExampleTask3();
+                        break;
+
+                    case "demo4": // fine
+                        await ExampleTask4();
+                        break;
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                // success, this is what we want. Deal with the exception
+                Console.WriteLine(e);
+                MessageBox.Show("Success!");
+            }
+
+            switch (caseToRun)
+            {
+                case "demo5": // Pro crash (unobserved task exception)
+                    Example5();
+                    break;
+            }
+
+        }
+
+        private static Task<bool> ExampleTask1()
+        {
+            return QueuedTask.Run<bool>(() =>
+            {
+                var i = 0;
+                i = i / 0;
+                return i > 0;
+            });
+        }
+
+        private static async void ExampleTask2()
+        {
+            await QueuedTask.Run<bool>(() =>
+            {
+                var i = 0;
+                i = i / 0;
+                return i > 0;
+            });
+
+            await Task.Delay(2000);
+
+        }
+
+        private static async void ExampleTask3()
+        {
+
+            await ThrowAsync();
+        }
+
+
+
+        static async Task ExampleTask4()
+        {
+
+            await ThrowAsync();
+        }
+
+        static void Example5()
+        {
+
+            throw new Exception("An error occurred");
+        }
+
+        // Helper that sets an exception async
+        static Task ThrowAsync()
+        {
+            var tcs = new TaskCompletionSource<string>();
+            ThreadPool.QueueUserWorkItem(_ => tcs.SetException(new Exception("ThrowAsync")));
+            return tcs.Task;
+        }
+    }
+}
